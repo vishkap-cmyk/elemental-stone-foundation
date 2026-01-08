6ee279926259ed36d94b92ac1e6fca27331d97f3
vishkap-cmyk <vishkap@gmail.com>
2026-01-08 18:36:48 +0000

    Update Approach.tsx

--- src/pages/Approach.tsx
+++ src/pages/Approach.tsx
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet-async";
 import Layout from "@/components/Layout";
 
 const Approach = () => {
-  return (
+  return ( 
     <>
       <Helmet>
         <title>Approach — Elemental</title>
